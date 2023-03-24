e36b051a3ae9862780a0d12675b1de538eb1be08
Mohammed Osama <Mohammedabuelwafa@aucegypt.edu>
2023-03-24 22:15:41 +0200

    Implemented Main.java
    This class is the driver for the snake game, and it has the game loop. it may need some refactoring to overcome static code analysis warnings.

--- Snake/src/Main.java
+++ Snake/src/Main.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+
+        //initialize the game
+        GameBoard gameBoard = initializeGame();
+
+        //Game loop
+        while (true) {
+            gameBoard.update();
+            gameBoard.repaint();
+            Thread.sleep(GameBoard.FRAME_DELAY);
+        }
+    }
+
+    private static GameBoard initializeGame() {
+        JFrame frame = new JFrame("Snake");
+        GameBoard gameBoard = new GameBoard();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(gameBoard);
+        frame.pack();
+        frame.setVisible(true);
+        return gameBoard;
     }
 }
